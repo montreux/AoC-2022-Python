4071f763262059131ccb172d604a3655cf91d5d6
John Holcroft <john.holcroft@montreux.co.uk>
2023-06-26 12:37:49 +0100

    Part 1 complete

--- Day02/test_day02.py
+++ Day02/test_day02.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day02_data import EXAMPLE_INPUT
+from day02_data import EXAMPLE_INPUT, PUZZLE_INPUT
 from day02 import score_match
 
 
@@ -8,6 +8,10 @@ class TestElfRockPaperScissors(unittest.TestCase):
     def test_score_round_part_one_example_data(self):
         expected_output = 15
         self.assertEqual(score_match(EXAMPLE_INPUT), expected_output)
+
+    def test_score_round_part_one_puzzle_data(self):
+        expected_output = 11906
+        self.assertEqual(score_match(PUZZLE_INPUT), expected_output)
 
 
 if __name__ == "__main__":
